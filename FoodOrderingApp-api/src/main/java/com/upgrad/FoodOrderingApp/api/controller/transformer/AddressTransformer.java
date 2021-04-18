8d9026c7e4c016720b854478b0835cab875bed22
“skm0078” <skmukherjee1994@gmail.com>
2021-04-18 22:28:26 +0530

    Authentication added , other branches merged

---
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/transformer/AddressTransformer.java
@@ -0,0 +1,47 @@
+package com.upgrad.FoodOrderingApp.api.controller.transformer;
+
+import com.upgrad.FoodOrderingApp.api.model.*;
+import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+public final class AddressTransformer {
+
+    public static StatesListResponse toStateListResponse(List<StateEntity> stateEntityList) {
+        StatesListResponse stateListResponse = new StatesListResponse();
+        // iterate stateEntityList parameter and add to StatesListResponse
+        stateEntityList.forEach(
+                se -> {
+                    StatesList statesItem = new StatesList(); // create new entity add to the List
+                    statesItem.setStateName(se.getState_name());
+                    statesItem.setId(UUID.fromString(se.getUuid()));
+                    stateListResponse.addStatesItem(statesItem);
+                });
+        return stateListResponse;
+    }
+
+    public static AddressListResponse toAddressListResponse(List<AddressEntity> addressEntityList) {
+        AddressListResponse addressListResponse = new AddressListResponse();
+
+        // iterate addressEntityList and add each address Item to the addressListResponse
+
+        for (AddressEntity ae : addressEntityList) {
+            AddressListState addressListState = new AddressListState();
+            addressListState.setStateName(ae.getState().getState_name()); // get StateName from state obj
+            // create AddressList Object and add it to the addressEntityList which is the final response
+            AddressList addressList =
+                    new AddressList()
+                            .id(UUID.fromString(ae.getUuid()))
+                            .city(ae.getCity())
+                            .flatBuildingName(ae.getFlatBuilNo())
+                            .locality(ae.getLocality())
+                            .pincode(ae.getPincode())
+                            .state(addressListState);
+            addressListResponse.addAddressesItem(addressList);
+        }
+
+        return addressListResponse; // return the final response
+    }
+}
